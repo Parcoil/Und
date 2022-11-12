755702a3ab62bc0f73f48a81b06ed75a764f8704
Minoa Baccus <57844837+MinoaBaccus@users.noreply.github.com>
2022-11-12 01:51:24 -0600

    bruhh

--- js/main.js
+++ js/main.js
@@ -36,6 +36,11 @@ const tabCloak = document.createElement("script");
 tabCloak.setAttribute("src", "/js/tab_cloak.js");
 document.head.append(tabCloak);
 script("Injected script 2/5");
+
+const arc = document.createElement("script");
+arc.setAttribute("async", "");
+arc.setAttribute("src", "");
+document.head.append(arc);
 script("Injected script 3/5");
 
 const adblock = document.createElement("script");
