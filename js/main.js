acee1a31c811405e2b0d5ab0bc872acc20cb39ea
Minoa <57844837+MinoaBaccus@users.noreply.github.com>
2022-11-15 15:36:24 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ document.head.append(arc);
 script("Injected script 3/5");
 
 const adblock = document.createElement("script");
-adblock.setAttribute("src", "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1");
+adblock.setAttribute("src", "");
 adblock.setAttribute("nonce", "yibq-w_TR5NOCRWsU-VL0Q");
 adblock.setAttribute("async", "");
 document.head.append(adblock);
@@ -43,7 +43,7 @@ script("Injected script 4/5");
 
 const ad = document.createElement("script");
 ad.setAttribute("async", "");
-ad.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5756835229788588");
+ad.setAttribute("src", "");
 ad.setAttribute("crossorigin", "anonymous");
 document.head.append(ad);
 script("Injected script 3/5");
