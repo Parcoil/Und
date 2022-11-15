83d25f42797064b10734e91877d3fb34caeee8f4
Minoa <57844837+MinoaBaccus@users.noreply.github.com>
2022-11-15 10:41:37 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 console.warn(
   "%cNote!",
   "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io\nPlease do not just add them without asking me first! Thank you!"
+  "Thank you for playing on mettet.ml!"
 );
 
 function script(text) {
@@ -20,15 +20,6 @@ var gaenabled = window.localStorage.getItem("ga");
 if (gaenabled == "false") {
   script("Skipped GA injection because it is disabled by the user.");
 } else {
-  const gascript = document.createElement("script");
-  gascript.setAttribute("async", "");
-  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
-  const inlinegascript = document.createElement("script");
-  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-98DP5VKS42');`;
-  document.head.append(gascript, inlinegascript);
   script("Injected script 1/5");
 }
 
